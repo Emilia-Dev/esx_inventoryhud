13d3e8a7935e4a8aa1bc0d93914af361a247bc82
Emilia <60162373+Emilia-Dev@users.noreply.github.com>
2020-02-07 09:12:19 +0700

    Add files via upload

---
+++ html/locales/en.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "<span class=\"emoji\">🗑️</span> ทิ้ง";
+invLocale.useItem = "<span class=\"emoji\">🖐️</span> ใช้";
+invLocale.giveItem = "<span class=\"emoji\">🎁</span> ให้";
+invLocale.secondInventoryNotAvailable = "คลังสินค้าที่สองไม่สามารถใช้ได้";
